080c5ffb019c6e98dfcd42b483a294a08c1eeeaa
matheusavigo <matheusavigo@gmail.com>
2026-01-15 08:46:58 -0300

    terceiro commit

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ if (quantidadeXp <= 1000) {
     nivel = "Bronze"
 } else if (quantidadeXp > 2000 && quantidadeXp <= 5000) {
     nivel = "Prata"
+} else if (quantidadeXp > 5000 && quantidadeXp <= 7000) {
+    nivel = "Ouro"
+} else if (quantidadeXp > 7000 && quantidadeXp <= 8000) {
+    nivel = "Platina"
+} else if (quantidadeXp > 8000 && quantidadeXp <= 9000) {
+    nivel = "Ascendente"
+} else if (quantidadeXp > 9000 && quantidadeXp <= 10000) {
+    nivel = "Imortal"
+} else {
+    nivel = "Radiante"
 }
