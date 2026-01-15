6170c48b153ac478a1d97a86900140d0908b2d98
matheusavigo <matheusavigo@gmail.com>
2026-01-14 21:35:02 -0300

    segundo commmit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 let nomeHeroi = "steve"
-let quantidadeXp = 8000
+let quantidadeXp = 900
+let nivel = ""
 
+if (quantidadeXp <= 1000) {
+    nivel = "Ferro"
+} else if (quantidadeXp > 1000 && quantidadeXp <= 2000) {
+    nivel = "Bronze"
+} else if (quantidadeXp > 2000 && quantidadeXp <= 5000) {
+    nivel = "Prata"
+}
